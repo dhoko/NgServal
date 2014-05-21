6b1a70ca2fad942bd5e385e3f606611116244a1a
dhoko <aurelien@procheo.fr>
2014-05-21 10:59:25 +0200

    Module i18n ready

--- app/scripts/core/core.js
+++ app/scripts/core/core.js
@@ -1,11 +1,13 @@
-var core = angular.module('ngStarter', ['ui.router']);
+var i18n = require('../i18n/i18n');
+
+var core = angular.module('ngStarter', ['ui.router','i18n']);
 
 core.config(require('./coreConfig'))
     .controller("rootCtrl", require('./controllers/root'))
     .controller("welcomeCtrl", require('./controllers/welcome'))
-    .factory('localize', require('./services/i18n'))
+    // .factory('localize', require('./services/i18n'))
     .factory('actions', require('./services/actions'))
-    .run(['localize','actions', '$state', function(localize, actions, $state) {
-        localize.load();
+    .run(['actions', '$state', function(actions, $state) {
+        // localize.load();
         actions.init();
     }]);
